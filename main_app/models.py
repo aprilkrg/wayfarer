e541d148e8d6048b4544901037c89fce3c150977
ADamian17 <adonisdesuvikdo@gmail.com>
2020-10-08 10:08:12 -0700

    added models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField( max_length=100 )
+    image = models.TextField()
+    country = models.CharField( max_length=100 )
+
+class Profile(models.Model):
+    user_id = models.OneToOneField( User, on_delete=models.CASCADE )
+    current_city = models.OneToOneField( City, on_delete=models.CASCADE )
+    created_at = models.DateTimeField( auto_now_add=True )
+    updated_at = models.DateTimeField( auto_now=True )
+
+class Post(models.Model):
+    city_id = models.OneToOneField( City, on_delete=models.CASCADE, related_name="city"  )
+    user_id = models.OneToOneField( User, on_delete=models.CASCADE, related_name="user" )
