46637a30b6e697dc263d3ee1e2951fd1bedd6a70
ADamian17 <adonisdesuvikdo@gmail.com>
2020-10-09 11:23:58 -0700

    working in register

--- main_app/views.py
+++ main_app/views.py
@@ -16,3 +16,34 @@ def home( request ):
 
 def cities( request ):
     return render(request, 'city/index.html') 
+
+
+
+
+# def profile_edit(request):
+#     user = request.user
+#     if request.method == 'POST':
+#         try:
+#             profile_form = Profile_Form(request.POST, request.FILES, instance=user.profile)
+#             if profile_form.is_valid():
+#                 new_profile = profile_form.save(commit=False)
+#                 new_profile.user = request.user
+#                 profile.image = request.FILES['image']
+#                 new_profile.save()
+#         except:
+#             profile_form = Profile_Form(request.POST, request.FILES)
+#             if profile_form.is_valid():
+#                 new_profile = profile_form.save(commit=False)
+#                 new_profile.user = request.user
+#                 profile.image = request.FILES['image']
+#                 new_profile.save()
+#         return redirect('profile')
+#     else:
+#         try:
+#             profile_form = Profile_Form(instance=user.profile)
+#             context = {'profile_form': profile_form}
+#             return render(request, 'account/edit.html', context)
+#         except:
+#             profile_form = Profile_Form()
+#             context = {'profile_form': profile_form}
+#             return render(request, 'account/edit.html', context)    
