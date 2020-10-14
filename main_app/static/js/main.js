578cfc47a10de0918e35ca521b1ef0eccc7239bf
Adonis D Martin <adonisdesuvikdo@gmail.com>
2020-10-14 14:40:24 -0700

    Adonis/is activecity (#22)
    * cleaning a little bit
    
    * working in reusables components
    
    * added buttons styles
    
    * added more styles, see you project in a few weeks

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,8 +1,8 @@
 const $btn = $('#post_edit');
 const $cardFormEdit = $('#edit-post');
+const $sidebar = $('.city-sidebar')
 
-$btn.on('click', (e) => {
-    
+$btn.on('click', (e) => {  
     if ( $cardFormEdit.hasClass('animate') ) {
         $cardFormEdit.css( { visibility: 'hidden' });
         $cardFormEdit.removeClass('animate'); 
