68bf20af39eceb7b001c271f7afdc7389206b22c
Adonis D Martin <adonisdesuvikdo@gmail.com>
2020-10-14 08:55:01 -0700

    Adonis/ dashstyles (#21)
    * added closing edit form
    
    * added city sidebar styles

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -2,6 +2,12 @@ const $btn = $('#post_edit');
 const $cardFormEdit = $('#edit-post');
 
 $btn.on('click', (e) => {
-    $cardFormEdit.css( { visibility: 'visible' });
-    $cardFormEdit.addClass('animate');
+    
+    if ( $cardFormEdit.hasClass('animate') ) {
+        $cardFormEdit.css( { visibility: 'hidden' });
+        $cardFormEdit.removeClass('animate'); 
+    } else {
+        $cardFormEdit.css( { visibility: 'visible' });
+        $cardFormEdit.addClass('animate');
+    }
 });
